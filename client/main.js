59bc6ce090b965f787ff08a9ba95e6e924f1b396
kndllbrwn <kndllbrwn1914@gmail.com>
2017-04-30 21:21:01 -0400

    iterating over array of objects example

--- client/main.js
+++ client/main.js
@@ -2,6 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Meteor} from 'meteor/meteor'
 
+const players = [{
+  _id: '1',
+  name: 'Tom',
+  score: 99
+},
+{
+  _id: '2',
+  name: 'Dick',
+  score: -1
+},
+{
+  _id: '3',
+  name: 'Harry',
+  score: -12
+}]
+
+const renderPlayers = players.map((player) => {
+  return <p key={player._id}>{`${player.name} has ${player.score} point(s).`}</p>
+})
+
 Meteor.startup(function () {
   const title = 'Been there'
   const name = 'Done that'
@@ -10,6 +30,7 @@ Meteor.startup(function () {
     <h1>{title}</h1>
     <p>And {name}!</p>
     <p>Me too!</p>
+    {renderPlayers}
     </div>)
   ReactDOM.render(jsx, document.getElementById('app'))
 })
