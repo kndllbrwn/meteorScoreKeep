30d28d7965785e77d3d0b144ec24444d69fda612
kndllbrwn <kndllbrwn1914@gmail.com>
2017-04-30 20:55:48 -0400

    Added react and created first component

--- client/main.js
+++ client/main.js
@@ -1,5 +1,15 @@
-import someDefault, { greetUser, name } from '../imports/utils'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Meteor} from 'meteor/meteor'
 
-console.log('Log from /client/main.js')
-console.log(greetUser())
-console.log(someDefault)
+Meteor.startup(function () {
+  const title = 'Been there'
+  const name = 'Done that'
+  const jsx = (
+    <div>
+    <h1>{title}</h1>
+    <p>And {name}!</p>
+    <p>Me too!</p>
+    </div>)
+  ReactDOM.render(jsx, document.getElementById('app'))
+})
